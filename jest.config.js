4ae40bd31a2c32d27b99def5d69cc233038cc304
Aneela Chagarlamudi <achagarl@redhat.com>
2024-03-27 10:22:06 -0400

    Remove unused files and update packages

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,21 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js', '!src/**/stories/*'],
   coverageDirectory: './coverage/',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/config/fileMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy'
   },
-  roots: ['<rootDir>/src/'],
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(?!(lodash-es|@redhat-cloud-services|@openshift|@patternfly|uuid))',
   ],
-  testEnvironment: 'jest-environment-jsdom',
-  moduleDirectories: [
-    'node_modules',
-    './src', //the root directory
-  ],
+  transform: {
+    '^.+\\.(ts|js)x?$': ['ts-jest'],
+    '\\.(jpg|jpeg|png|gif|svg)$': [
+      '<rootDir>/fileTransformer.js',
+    ],
+  },
   setupFilesAfterEnv: ['<rootDir>/config/jest.setup.js'],
 };
