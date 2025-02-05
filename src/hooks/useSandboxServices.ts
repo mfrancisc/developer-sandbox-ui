2044d2b70e909ecfdb1751dbb0824a83e8888ade
Devtools <devtools@redhat.com>
2025-02-05 09:11:03 +0100

    updates to the aap cr

--- src/hooks/useSandboxServices.ts
+++ src/hooks/useSandboxServices.ts
@@ -102,284 +102,303 @@ export const useSandboxServices = (handleShowAAPModal: () => void): Service[] =>
 // This is custom crafted AAP CR which is able to run properly with the sandbox environment constraints.
 // In future release, some of these values might become defaults or they will be moved into webhooks.
 // For the time being, they are being stored here in order to simplify the configuration and deployment for the end user.
+
 const AAPObject: string = `
 {
-  "apiVersion": "aap.ansible.com/v1alpha1",
-  "kind": "AnsibleAutomationPlatform",
-  "metadata": {
-    "name": "sandbox-aap"
-  },
-  "spec": {
-    "no_log": false,
-    "api": {
-      "replicas": 1,
-      "resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "256Mi"
-        },
-        "limits": {
-          "cpu": "500m",
-          "memory": "1000Mi"
-        }
+   "apiVersion":"aap.ansible.com/v1alpha1",
+   "kind":"AnsibleAutomationPlatform",
+   "metadata":{
+      "name":"sandbox-aap"
+   },
+   "spec":{
+      "no_log":false,
+      "api":{
+         "replicas":1,
+         "resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"256Mi"
+            },
+            "limits":{
+               "cpu":"500m",
+               "memory":"1000Mi"
+            }
+         }
+      },
+      "redis":{
+         "replicas":1,
+         "resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"256Mi"
+            },
+            "limits":{
+               "cpu":"500m",
+               "memory":"500Mi"
+            }
+         }
+      },
+      "database":{
+         "replicas":1,
+         "resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"256Mi"
+            },
+            "limits":{
+               "cpu":"500m",
+               "memory":"800Mi"
+            }
+         }
+      },
+      "controller":{
+         "extra_settings":[
+            {
+               "setting":"DEFAULT_EXECUTION_QUEUE_POD_SPEC_OVERRIDE",
+               "value":{
+                  "resources":{
+                     "limits":{
+                        "cpu":"200m",
+                        "memory":"500Mi"
+                     },
+                     "requests":{
+                        "cpu":"220m",
+                        "memory":"100Mi"
+                     }
+                  }
+               }
+            }
+         ],
+         "garbage_collect_secrets":true,
+         "disabled":false,
+         "uwsgi_processes":2,
+         "task_resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"150Mi"
+            },
+            "limits":{
+               "cpu":"1000m",
+               "memory":"1200Mi"
+            }
+         },
+         "web_resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"200Mi"
+            },
+            "limits":{
+               "cpu":"200m",
+               "memory":"1600Mi"
+            }
+         },
+         "ee_resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"64Mi"
+            },
+            "limits":{
+               "cpu":"1000m",
+               "memory":"500Mi"
+            }
+         },
+         "redis_resource_requirements":{
+            "requests":{
+               "cpu":"50m",
+               "memory":"64Mi"
+            },
+            "limits":{
+               "cpu":"100m",
+               "memory":"200Mi"
+            }
+         },
+         "rsyslog_resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"128Mi"
+            },
+            "limits":{
+               "cpu":"500m",
+               "memory":"250Mi"
+            }
+         },
+         "init_container_resource_requirements":{
+            "requests":{
+               "cpu":"100m",
+               "memory":"128Mi"
+            },
+            "limits":{
+               "cpu":"500m",
+               "memory":"200Mi"
+            }
+         }
+      },
+      "eda":{
+         "disabled":false,
+         "api":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"50m",
+                  "memory":"350Mi"
+               },
+               "limits":{
+                  "cpu":"500m",
+                  "memory":"400Mi"
+               }
+            }
+         },
+         "ui":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"25m",
+                  "memory":"64Mi"
+               },
+               "limits":{
+                  "cpu":"500m",
+                  "memory":"150Mi"
+               }
+            }
+         },
+         "scheduler":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"50m",
+                  "memory":"200Mi"
+               },
+               "limits":{
+                  "cpu":"500m",
+                  "memory":"250Mi"
+               }
+            }
+         },
+         "worker":{
+            "replicas":2,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"25m",
+                  "memory":"200Mi"
+               },
+               "limits":{
+                  "cpu":"250m",
+                  "memory":"250Mi"
+               }
+            }
+         },
+         "default_worker":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"25m",
+                  "memory":"200Mi"
+               },
+               "limits":{
+                  "cpu":"500m",
+                  "memory":"400Mi"
+               }
+            }
+         },
+         "activation_worker":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"25m",
+                  "memory":"150Mi"
+               },
+               "limits":{
+                  "cpu":"500m",
+                  "memory":"400Mi"
+               }
+            }
+         },
+         "event_stream":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"25m",
+                  "memory":"150Mi"
+               },
+               "limits":{
+                  "cpu":"100m",
+                  "memory":"300Mi"
+               }
+            }
+         }
+      },
+      "hub":{
+         "redis_data_persistence":false,
+         "disabled":false,
+         "storage_type":"file",
+         "file_storage_storage_class":"efs-sc",
+         "file_storage_size":"10Gi",
+         "api":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"150m",
+                  "memory":"256Mi"
+               },
+               "limits":{
+                  "cpu":"800m",
+                  "memory":"500Mi"
+               }
+            }
+         },
+         "content":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"150m",
+                  "memory":"256Mi"
+               },
+               "limits":{
+                  "cpu":"800m",
+                  "memory":"1200Mi"
+               }
+            }
+         },
+         "worker":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"150m",
+                  "memory":"256Mi"
+               },
+               "limits":{
+                  "cpu":"800m",
+                  "memory":"400Mi"
+               }
+            }
+         },
+         "web":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"100m",
+                  "memory":"256Mi"
+               },
+               "limits":{
+                  "cpu":"500m",
+                  "memory":"300Mi"
+               }
+            }
+         },
+         "redis":{
+            "replicas":1,
+            "resource_requirements":{
+               "requests":{
+                  "cpu":"100m",
+                  "memory":"250Mi"
+               },
+               "limits":{
+                  "cpu":"300m",
+                  "memory":"400Mi"
+               }
+            }
+         }
       }
-    },
-    "redis": {
-      "replicas": 1,
-      "resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "256Mi"
-        },
-        "limits": {
-          "cpu": "500m",
-          "memory": "500Mi"
-        }
-      }
-    },
-    "database": {
-      "replicas": 1,
-      "resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "256Mi"
-        },
-        "limits": {
-          "cpu": "500m",
-          "memory": "800Mi"
-        }
-      }
-    },
-    "controller": {
-      "garbage_collect_secrets": true,
-      "disabled": false,
-      "uwsgi_processes": 2,
-      "task_resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "150Mi"
-        },
-        "limits": {
-          "cpu": "1000m",
-          "memory": "1200Mi"
-        }
-      },
-      "web_resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "200Mi"
-        },
-        "limits": {
-          "cpu": "200m",
-          "memory": "1600Mi"
-        }
-      },
-      "ee_resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "64Mi"
-        },
-        "limits": {
-          "cpu": "1000m",
-          "memory": "500Mi"
-        }
-      },
-      "redis_resource_requirements": {
-        "requests": {
-          "cpu": "50m",
-          "memory": "64Mi"
-        },
-        "limits": {
-          "cpu": "100m",
-          "memory": "200Mi"
-        }
-      },
-      "rsyslog_resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "128Mi"
-        },
-        "limits": {
-          "cpu": "500m",
-          "memory": "250Mi"
-        }
-      },
-      "init_container_resource_requirements": {
-        "requests": {
-          "cpu": "100m",
-          "memory": "128Mi"
-        },
-        "limits": {
-          "cpu": "500m",
-          "memory": "200Mi"
-        }
-      }
-    },
-    "eda": {
-      "disabled": false,
-      "api": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "50m",
-            "memory": "350Mi"
-          },
-          "limits": {
-            "cpu": "500m",
-            "memory": "400Mi"
-          }
-        }
-      },
-      "ui": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "25m",
-            "memory": "64Mi"
-          },
-          "limits": {
-            "cpu": "500m",
-            "memory": "150Mi"
-          }
-        }
-      },
-      "scheduler": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "50m",
-            "memory": "200Mi"
-          },
-          "limits": {
-            "cpu": "500m",
-            "memory": "250Mi"
-          }
-        }
-      },
-      "worker": {
-        "replicas": 2,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "25m",
-            "memory": "200Mi"
-          },
-          "limits": {
-            "cpu": "250m",
-            "memory": "250Mi"
-          }
-        }
-      },
-      "default_worker": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "25m",
-            "memory": "200Mi"
-          },
-          "limits": {
-            "cpu": "500m",
-            "memory": "400Mi"
-          }
-        }
-      },
-      "activation_worker": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "25m",
-            "memory": "150Mi"
-          },
-          "limits": {
-            "cpu": "500m",
-            "memory": "400Mi"
-          }
-        }
-      },
-      "event_stream": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "25m",
-            "memory": "150Mi"
-          },
-          "limits": {
-            "cpu": "100m",
-            "memory": "300Mi"
-          }
-        }
-      }
-    },
-    "hub": {
-      "disabled": false,
-      "storage_type": "file",
-      "file_storage_storage_class": "efs-sc",
-      "file_storage_size": "10Gi",
-      "api": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "150m",
-            "memory": "256Mi"
-          },
-          "limits": {
-            "cpu": "800m",
-            "memory": "500Mi"
-          }
-        }
-      },
-      "content": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "150m",
-            "memory": "256Mi"
-          },
-          "limits": {
-            "cpu": "800m",
-            "memory": "1200Mi"
-          }
-        }
-      },
-      "worker": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "150m",
-            "memory": "256Mi"
-          },
-          "limits": {
-            "cpu": "800m",
-            "memory": "400Mi"
-          }
-        }
-      },
-      "web": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "100m",
-            "memory": "256Mi"
-          },
-          "limits": {
-            "cpu": "500m",
-            "memory": "300Mi"
-          }
-        }
-      },
-      "redis": {
-        "replicas": 1,
-        "resource_requirements": {
-          "requests": {
-            "cpu": "100m",
-            "memory": "250Mi"
-          },
-          "limits": {
-            "cpu": "300m",
-            "memory": "400Mi"
-          }
-        }
-      }
-    }
-  }
+   }
 }
 `
