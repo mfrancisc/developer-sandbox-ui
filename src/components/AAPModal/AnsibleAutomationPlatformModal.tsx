f4b11f5d943a985486ae9c8d028a92590e3e6d4d
Devtools <devtools@redhat.com>
2025-03-17 19:23:36 +0100

    make close buttons primary

--- src/components/AAPModal/AnsibleAutomationPlatformModal.tsx
+++ src/components/AAPModal/AnsibleAutomationPlatformModal.tsx
@@ -83,7 +83,7 @@ function AnsibleAutomationPlatformProvisioningModalContent(props: {
       />
       <br />
       <Button
-        variant="link"
+        variant="primary"
         size="sm"
         component="span"
         isInline
@@ -218,7 +218,7 @@ function AnsibleAutomationPlatformReadyModalContent(props: {
       </TextContent>
       <br />
       <Button
-        variant="secondary"
+        variant="primary"
         size="sm"
         component="span"
         isInline
