665e77343c541e34d675599fdae9833e685d4abf
Devtools <devtools@redhat.com>
2025-05-04 16:32:08 +0200

    WIP add redirect functionality

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -1,3 +1,16 @@
+import { canaryDeploymentCheck } from './utils/canary-deployment';
+/*** CANARY Deployment for redirecting users to new UI
+ Users visiting the first time the UI will get assigned with a random number, the number will be stored in localstorage and reused everytime the user opens the UI.
+ The users that gets a number that is lower than a canary weight threshold, they will be redirected to the new ui,
+ the others will be using the current UI.
+ The threshold can be configured in the backend so that a bigger/smaller number of users can be routed to the new UI.
+ ***/
+// TODO get the threshold from the backend regsvc
+const UI_CANARY_WEIGHT_TRESHOLD = 95;
+canaryDeploymentCheck(UI_CANARY_WEIGHT_TRESHOLD);
+/** END canary UI deployment **/
+
+
 import React from 'react';
 import App from './App';
 
---
+++ src/utils/canary-deployment.ts
@@ -0,0 +1,31 @@
+export const randomIntFromInterval = (min: number, max: number): number => { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const redirectUser = (userWeightValue: number, weightThreshold: number) => {
+  if (userWeightValue <= weightThreshold) {
+    console.log('user will be redirected to new UI');
+    window.location.href = 'https://sandbox.redhat.com';
+    return
+  }
+  console.log('user will NOT be redirected to new UI.');
+};
+
+export const canaryDeploymentCheck = (canaryWeightThreshold: number) => {
+// check if user already has the UI canary deployment weight
+  const UICanaryWeightJson = localStorage.getItem('dev-sandbox.ui-canary-weight');
+  if (UICanaryWeightJson == null) {
+    // generate a new rand number for the user
+    const rndInt = randomIntFromInterval(1, 100);
+    // store the new rand number for the user in the local storage as a string
+    localStorage.setItem('dev-sandbox.ui-canary-weight', JSON.stringify(rndInt)); // numbers must be saved as strings in localstorage
+    console.log('dev-sandbox.ui-canary-weight', rndInt);
+    // check if user should go to new UI and redirect in case
+    redirectUser(rndInt, canaryWeightThreshold);
+  } else {
+    // convert the string back to int
+    const UICanaryWeight = JSON.parse(UICanaryWeightJson);
+    console.log('dev-sandbox.ui-canary-weight', UICanaryWeight);
+    redirectUser(UICanaryWeight, canaryWeightThreshold);
+  }
+};
